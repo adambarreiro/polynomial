e79a275c2fbaf17148f27427b4cc1ee7cf040364
adambarreiro <adambarreiro@gmail.com>
2014-05-10 13:06:32 +0200

    Manuales

--- database/admins.js
+++ database/admins.js
@@ -1,1 +1,1 @@
-{ "user" : "chema", "password" : "ozAHxKPiyXNT4+FdCogSDxF3dj6gs/sVcVZ1PjGnT4E=", "_id" : { "$oid" : "526cebef68d3aa5503000001" }, "__v" : 0 }
+{ "user" : "admin", "password" : "ozAHxKPiyXNT4+FdCogSDxF3dj6gs/sVcVZ1PjGnT4E=", "_id" : { "$oid" : "526cebef68d3aa5503000001" }, "__v" : 0 }
